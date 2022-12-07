42e9ff0e2e3beb51af3a73ebb712610b922b7c77
Viirz <virganantasinau@gmail.com>
2022-12-07 22:45:14 +0700

    little update

--- PemDas/src/Tugas/TP_225150201111028_VirganantaSaputra_1.java
+++ PemDas/src/Tugas/TP_225150201111028_VirganantaSaputra_1.java
@@ -1,8 +1,14 @@
+/**
+ Nama 1: Virgananta Saputra (NIM 225150201111028)
+ Nama 2: Muhammad Nadhil Luthfirrahman (NIM 225150207111049)
+ Tanggung jawab 1: mengerjakan soal c, membuat codingan, format file, dan diskusi jawban d dan e
+ Tanggung jawab 2: mengerjakan soal a dan b, membuat pseudocode, membuat flowchart, dan diskusi jawaban d dan e
+ **/
 package Tugas;
 
 import java.util.Scanner;
 
-public class TugasAkhir {
+public class TP_225150201111028_VirganantaSaputra_1 {
     final static int MAX_SISWA = 10;
     final static int MAX_MATPEL = 10;
     final static int MAX_SEMESTER = 2;
@@ -17,79 +23,9 @@ public class TugasAkhir {
     // menyimpan banyak matpel
     static int banyakSiswaMatpel = 0;
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String[] perintah = new String[500];
-        for (int i = 0; i < perintah.length; i++) {
-            String masukan = input.nextLine();
-            if (masukan.equals("")) {
-                break;
-            }
-            perintah[i] = masukan;
-        }
-        for (int j = 0; j < perintah.length; j++) {
-            if (perintah[j]==null)
-                continue;
-            String[] jenisPerintah = perintah[j].split(" ");
-            switch (jenisPerintah[0]) {
-                case "SISWA" -> {
-                    String nama = jenisPerintah[1];
-                    tambahSiswa(nama);
-                }
-                case "MATPEL" -> {
-                    String matpel = jenisPerintah[1];
-                    tambahMatpel(matpel);
-                }
-                case "NILAI" -> {
-                    String nama = jenisPerintah[1];
-                    String semestberapa = jenisPerintah[2];
-                    int semester = 0;
-                    if (semestberapa.equals("GENAP"))
-                        semester = 1;
-                    String matpel = jenisPerintah[3];
-                    int nilai = Integer.parseInt(jenisPerintah[4]);
-                    isiDataSiswa(nama,semester,matpel,nilai);
-                }
-                case "PRINT_SISWA" -> printSiswa();
-                case "PRINT_MATPEL" -> printMatpel();
-                case "PRINT_RAPORT" -> {
-                    String nama = jenisPerintah[1];
-                    printRaportSiswa(nama);
-                }
-                case "CARI_JUARA" -> {
-                    String semesterBerapa = jenisPerintah[1];
-                    int sum = 0;
-                    int semester = 1;
-                    if (semesterBerapa.equals("GANJIL")){
-                        semester = 0;
-                    }
-                    int nilaiTerbesar = cariJuara(semester);
-                    for (int k=0;k<banyakSiswa;k++){
-                        for (int l=0;l<banyakSiswaMatpel;l++){
-                            sum += siswaMatpelNilai[k][semester][l];
-                        }
-                        if (sum==nilaiTerbesar){
-                            System.out.println("JUARA_1 " + semesterBerapa + " " + siswa[k]);
-                        }
-                        sum = 0;
-                    }
-                }
-                case "HITUNG_NILAI" -> {
-                    String semesterBerapa = jenisPerintah[1];
-                    int semester = 1;
-                    if (semesterBerapa.equals("GANJIL")){
-                        semester = 0;
-                    }
-                    hitungNilai(semester);
-                }
-            }
-        }
-        
-    }
-
     public static String konversiNilai(double nilai){
         String nilaiHuruf="";
-        if (nilai > 80){
+        if (nilai > 80 && nilai < 100){
             nilaiHuruf = "A";
         } else if (nilai > 75 && nilai <= 80) {
             nilaiHuruf = "B+";
@@ -183,31 +119,29 @@ public class TugasAkhir {
         System.out.println("Semester: Ganjil");
         System.out.printf("%-20s|","Mata Pelajaran");
         System.out.printf("%10s|","Nilai");
-        System.out.printf("%10s\n","Nilai Huruf");
-        System.out.println("-------------------------------------------");
-        for(int i=0;i<=banyakSiswa;i++) {
+        System.out.printf("%15s\n","Nilai Huruf");
+        System.out.println("-----------------------------------------------");
+        for(int i=0;i<banyakSiswaMatpel;i++) {
             int indexSiswa = cariSiswa(nama);
             nilaiHuruf = konversiNilai(siswaMatpelNilai[indexSiswa][0][i]);
             System.out.printf("%-20s|",siswaMatpel[indexSiswa][0][i]);
             System.out.printf("%10.1f|", siswaMatpelNilai[indexSiswa][0][i]);
-            System.out.printf("%10s\n",nilaiHuruf);
+            System.out.printf("%15s\n",nilaiHuruf);
         }
-        System.out.println(">>END RAPORT<<");
         System.out.println("");
-        System.out.println("Nama: " + nama);
         System.out.println("Semester: Genap");
         System.out.printf("%-20s|","Mata Pelajaran");
         System.out.printf("%10s|","Nilai");
-        System.out.printf("%10s\n","Nilai Huruf");
-        System.out.println("-------------------------------------------");
-        for(int j=0;j<=banyakSiswa;j++) {
+        System.out.printf("%15s\n","Nilai Huruf");
+        System.out.println("-----------------------------------------------");
+        for(int j=0;j<banyakSiswaMatpel;j++) {
             int indexSiswa = cariSiswa(nama);
             nilaiHuruf = konversiNilai(siswaMatpelNilai[indexSiswa][1][j]);
             System.out.printf("%-20s|",siswaMatpel[indexSiswa][1][j]);
             System.out.printf("%10.1f|", siswaMatpelNilai[indexSiswa][1][j]);
-            System.out.printf("%10s\n",nilaiHuruf);
+            System.out.printf("%15s\n",nilaiHuruf);
         }
-        System.out.println(">>END RAPORT<<");
+        System.out.println("\n>>END RAPORT<<");
     }
     public static int cariJuara(int semester){
         int sum=0;int nilaiTerbesar=0;
@@ -222,6 +156,97 @@ public class TugasAkhir {
         return nilaiTerbesar;
     }
     public static void hitungNilai(int semester){
-
+        String nilaiHuruf = "";
+        String[] isiNilaiHuruf = new String[banyakSiswaMatpel];
+        String[] huruf = {"A","B+","B","C+","C","D+","D","E"};
+        int counter = 0;
+        for (int i=0;i<banyakSiswa;i++){
+            System.out.print(siswa[i] + " ");
+            for (int k=0;k<banyakSiswaMatpel;k++){
+                if (siswaMatpelNilai[i][semester][k]==0)
+                    continue;
+                double nilai = siswaMatpelNilai[i][semester][k];
+                nilaiHuruf = konversiNilai(nilai);
+                isiNilaiHuruf[k] = nilaiHuruf;
+            }
+            for (int k=0;k< huruf.length;k++) {
+                for (int l = 0; l < isiNilaiHuruf.length; l++) {
+                    if (isiNilaiHuruf[l].equals(huruf[k]))
+                            counter++;
+                }
+                if (counter > 0)
+                    System.out.printf("%s:%d ", huruf[k], counter);
+                counter = 0;
+            }
+            System.out.println("");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] perintah = new String[500];
+        for (int i = 0; i < perintah.length; i++) {
+            String masukan = input.nextLine();
+            if (masukan.equals("")) {
+                break;
+            }
+            perintah[i] = masukan;
+        }
+        for (int j = 0; j < perintah.length; j++) {
+            if (perintah[j]==null)
+                continue;
+            String[] jenisPerintah = perintah[j].split(" ");
+            switch (jenisPerintah[0]) {
+                case "SISWA" -> {
+                    String nama = jenisPerintah[1];
+                    tambahSiswa(nama);
+                }
+                case "MATPEL" -> {
+                    String matpel = jenisPerintah[1];
+                    tambahMatpel(matpel);
+                }
+                case "NILAI" -> {
+                    String nama = jenisPerintah[1];
+                    String semestberapa = jenisPerintah[2];
+                    int semester = 0;
+                    if (semestberapa.equals("GENAP"))
+                        semester = 1;
+                    String matpel = jenisPerintah[3];
+                    int nilai = Integer.parseInt(jenisPerintah[4]);
+                    isiDataSiswa(nama,semester,matpel,nilai);
+                }
+                case "PRINT_SISWA" -> printSiswa();
+                case "PRINT_MATPEL" -> printMatpel();
+                case "PRINT_RAPORT" -> {
+                    String nama = jenisPerintah[1];
+                    printRaportSiswa(nama);
+                }
+                case "CARI_JUARA" -> {
+                    String semesterBerapa = jenisPerintah[1];
+                    int sum = 0;
+                    int semester = 1;
+                    if (semesterBerapa.equals("GANJIL")){
+                        semester = 0;
+                    }
+                    int nilaiTerbesar = cariJuara(semester);
+                    for (int k=0;k<banyakSiswa;k++){
+                        for (int l=0;l<banyakSiswaMatpel;l++){
+                            sum += siswaMatpelNilai[k][semester][l];
+                        }
+                        if (sum==nilaiTerbesar){
+                            System.out.println("JUARA_1 " + semesterBerapa + " " + siswa[k]);
+                        }
+                        sum = 0;
+                    }
+                }
+                case "HITUNG_NILAI" -> {
+                    String semesterBerapa = jenisPerintah[1];
+                    int semester = 1;
+                    if (semesterBerapa.equals("GANJIL")){
+                        semester = 0;
+                    }
+                    hitungNilai(semester);
+                }
+            }
+        }
     }
 }
